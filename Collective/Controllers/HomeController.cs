a2a8b023135f012b11ba731a688d323d6042ad80
deliotclarke <deliotclarke@gmail.com>
2019-09-13 13:59:29 -0500

    wrote logic for upload, trying to get images folder to show up.

--- Collective/Controllers/HomeController.cs
+++ Collective/Controllers/HomeController.cs
@@ -11,6 +11,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Collective.Models.ProfileViewModel;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace Collective.Controllers
 {
@@ -22,13 +26,15 @@ namespace Collective.Controllers
         private readonly IConfiguration _config;
         private readonly SignInManager<ApplicationUser> _signInManger;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IHostingEnvironment _env;
 
-        public HomeController(ApplicationDbContext context, IConfiguration config, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        public HomeController(ApplicationDbContext context, IConfiguration config, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, IHostingEnvironment env)
         {
             _context = context;
             _config = config;
             _signInManger = signInManager;
             _userManager = userManager;
+            _env = env;
         }
 
         private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
@@ -60,6 +66,81 @@ namespace Collective.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost("ImageUpload")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ImageUpload(IFormFile file)
+        {
+            var user = await GetCurrentUserAsync();
+
+            if (ModelState.IsValid)
+            {
+                user.UserImgPath = await SaveFile(file, user.Id);
+
+                if (user.UserImgPath == null)
+                {
+                    return NotFound();
+                }
+
+                await _userManager.UpdateAsync(user);
+
+                return RedirectToAction(nameof(Profile));
+            }
+
+            return NotFound();
+        }
+
+        private async Task<string> SaveFile(IFormFile file, string userId)
+        {
+            if (file.Length > 5242880)
+            {
+                throw new Exception("File is too large! Try again!");
+            }
+
+            var ext = GetFileType(file.FileName);
+            if (ext == null)
+            {
+                throw new Exception("Incorrect file type, try again!");
+            }
+
+            var timestamp = new DateTimeOffset(DateTime.Now).ToUnixTimeMilliseconds();
+            var fileName = $"{timestamp}-{userId}.{ext}";
+
+            var webRoot = _env.WebRootPath;
+            var filePath = Path.Combine(webRoot, "images", fileName);
+
+            string relFilePath = null;
+            if (file.Length > 0)
+            {
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                    relFilePath = $"~/images/{filePath}";
+                }
+            }
+
+            return relFilePath;
+        }
+
+        private string GetFileType(string fileName)
+        {
+            var provider = new FileExtensionContentTypeProvider();
+
+            string contentType;
+            provider.TryGetContentType(fileName, out contentType);
+
+            if (contentType == "image/jpeg")
+            {
+                contentType = "jpg";
+            }
+            else
+            {
+                contentType = null;
+            }
+
+            return contentType;
         }
 
         public IActionResult Privacy()
